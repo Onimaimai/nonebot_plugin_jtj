092d30dac34d2a437f3af051b141d7656609b993
Onimai！ <93068569+Onimaimai@users.noreply.github.com>
2024-09-07 13:34:25 +0800

    Create arcade_data.py

---
+++ arcade_data.py
@@ -0,0 +1,192 @@
+def get_all_regions():
+    """获取所有存在的地区列表"""
+    return list(set([arcade["region"] for arcade in EMPTY_STATE]))
+
+EMPTY_STATE = [
+    
+    {
+      	"primary_keyword": "丁桥天街菲游乐",
+        "keywords": ["菲游乐","fyl","丁桥天街"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "新天地风云再起",
+        "keywords": ["新天地", "xtd"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+  	{
+      	"primary_keyword": "工联CC卡通尼",
+        "keywords": ["卡通尼", "cc"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+  	{
+      	"primary_keyword": "天空之城mai",
+        "keywords": ["天空mai", "tkmai"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "天空之城chu",
+        "keywords": ["天空chu", "tkchu"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+  	{
+      	"primary_keyword": "杭州东站量子空间",
+        "keywords": ["东站","hzd"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+  	{
+      	"primary_keyword": "湖滨银泰in77第一回合mai",
+        "keywords": ["77mai"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "湖滨银泰in77第一回合chu",
+        "keywords": ["77chu"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "临平余之城C站",
+        "keywords": ["c站","cz"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "拱墅万达萌可力mai",
+        "keywords": ["mklmai","萌mai","贡丸mai"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "拱墅万达萌可力chu",
+        "keywords": ["mklchu","萌chu","贡丸chu"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "拱墅宝龙游戏大魔方",
+        "keywords": ["gb","拱宝","大魔方","dmf"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "余杭万达宝贝王mai",
+        "keywords": ["鱼丸mai","余万mai"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "余杭万达宝贝王chu",
+        "keywords": ["鱼丸chu","余万chu"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "杭州"
+    },
+    {
+      	"primary_keyword": "武汉万象城真快活",
+        "keywords": ["武万"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "武汉"
+    },
+    {
+      	"primary_keyword": "台州天台祥生世纪广场",
+        "keywords": ["祥生","xs"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "台州"
+    },
+    {
+      	"primary_keyword": "银泰 POP BEAR",
+        "keywords": ["银"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "丽水"
+    },
+    {
+      	"primary_keyword": "西城广场天马行空潮玩",
+        "keywords": ["西"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "丽水"
+    },
+    {
+      	"primary_keyword": "中广未来城雄霸风云",
+        "keywords": ["霸"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "丽水"
+    },
+    {
+      	"primary_keyword": "青山湖宝龙七彩天空",
+        "keywords": ["q"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "临安"
+    },
+    {
+      	"primary_keyword": "钱王财富七彩天空",
+        "keywords": ["c"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "临安"
+    },
+    {
+      	"primary_keyword": "人民广场鱼乐游艺馆",
+        "keywords": ["y"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "临安"
+    },
+    {
+      	"primary_keyword": "临安宝龙七彩天空",
+        "keywords": ["b"],
+        "peopleCount": 0,
+        "updatedBy": "无",
+        "lastUpdatedAt": "04:00:00",
+        "region": "临安"
+    },
+  
+]
